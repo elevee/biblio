69ae32503239e0c35e33db923d820b710afb4711
Eric Levine <djlevine@gmail.com>
2013-07-12 14:35:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblio::Application.config.secret_token = '5c51b9e0bcad90f24c2b1b82edfb026a3f7a19c47ed4b47ec578010cf2f2b569c951fb66536e66f3a4be5723914d623dd36b795e091273ffd8f8694d1870678f'
